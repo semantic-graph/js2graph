0abebec1a05350c134589a0ed91fb983828d3ac3
Zhen Zhang <git@zhen-zhang.com>
2020-10-12 15:27:58 -0700

    Fix inter-proc flow

--- src/test/resources/small/example.js
+++ src/test/resources/small/example.js
@@ -1,37 +1,21 @@
-/*
-Expected output:
+function main() {
+  function f(x) {
+    return x;
+  }
 
-  1 -> console.log
+  function g(x, y) {
+    return x + y;
+  }
 
- */
+  function h() {
+    var x = f(1);
+    var y = g(2, 3);
+    return x + y;
+  }
 
-function f() {
-  return 1
+  console.log(h());
 }
 
-function g() {
-  return f() + 1;
-}
-
-function h() {
-  var x = f();
-  var y = g();
-  var z = x + y;
-  return z;
-}
-
-/*
-
-IR of this invocation in WALA:
-
-  12   v21 = global:global console             example.js [188->195] (line 23) [21=[$$destructure$rcvr4]]
-  13   check v21                               example.js [188->195] (line 23) [21=[$$destructure$rcvr4]]
-  BB2
-  16   v25 = global:global h                   example.js [200->201] (line 23)
-  17   check v25                               example.js [200->201] (line 23)
-  BB3
-  18   v27 = global:global __WALA__int3rnal__globalexample.js [200->203] (line 23)
-  19   v24 = invoke v25@19 v27 exception:v28   example.js [200->203] (line 23)
-  20   v23 = dispatch v22:#log@20 v21,v24 exception:v29example.js [188->204] (line 23) [22=[$$destructure$elt4]21=[$$destructure$rcvr4]]
- */
-console.log(h());
+// NOTE: need to put inside a main to ensure that getReturnFlowFunction will process h's exit to its call-site.
+// It might be a bug...I don't quite understand what is going on.
+main();
