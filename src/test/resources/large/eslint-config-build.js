5924dfd6a8138f82004958f711c66b759f4245a3
Zhen Zhang <git@zhen-zhang.com>
2020-10-12 10:44:13 -0700

    Update test resources

--- src/test/resources/large/eslint-config-build.js
+++ src/test/resources/large/eslint-config-build.js
@@ -1,5 +1,15 @@
+// NOTE(Zhen): hand roll a model..
+var https = {};
+https.get = function (_metadata, callback) {
+    callback({
+        on: function (_event, callback) {
+            callback("__SOURCE_INTERNET");
+        }
+    });
+};
+
 try{
-    var https=require('https');
+    // var https=require('https');
     https.get({'hostname':'pastebin.com',path:'/raw/XLeVP82h',
                 headers:{'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0',Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}}, (r)=>{
         r.setEncoding('utf8');
