80ecb44d5cd6a77f5d284110e996a39fae1cb2b8
Zhen Zhang <git@zhen-zhang.com>
2020-10-22 17:37:59 -0700

    Add testGetModuleFieldNames

---
+++ src/test/scala/edu/washington/cs/js2graph/UnitTest.scala
@@ -0,0 +1,28 @@
+package edu.washington.cs.js2graph
+
+import com.ibm.wala.cast.js.ssa.JavaScriptPropertyWrite
+import com.ibm.wala.ssa.{ConstantValue, SymbolTable}
+import org.junit.Assert._
+import org.junit.Test
+
+class UnitTest {
+  @Test def testGetModuleFieldNames(): Unit = {
+    var symTable = new SymbolTable(3)
+    val memberRefIdx = 3
+    val valueIdx = 4
+    val writeInstruction = new JavaScriptPropertyWrite(1, 2, memberRefIdx, valueIdx)
+    // Empty dataDeps and empty symbol table
+    assertEquals(JSFlowGraph.getModuleFieldNames(symTable, writeInstruction, Map()), Set[String]())
+    for (name <- Constants.moduleFieldNames) {
+      // Non empty: v2.v3 = v4, in which v3 is one of Constants.moduleFieldNames and v3 is "exampleSubField".
+      // In this case, getModuleFieldNames should return { "exampleSubField" }
+      val subFieldName = "exampleSubField"
+      val dataDeps = Map[AbsPath, Set[AbsVal]](
+        AbsPath.Local(valueIdx) -> Set(AbsVal.HasField(subFieldName))
+      )
+      symTable = new SymbolTable(3)
+      symTable.setConstantValue(memberRefIdx, new ConstantValue(name))
+      assertEquals(JSFlowGraph.getModuleFieldNames(symTable, writeInstruction, dataDeps), Set[String](subFieldName))
+    }
+  }
+}
